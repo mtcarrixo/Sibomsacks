bce99c6fea3851079ee1068d886794bb851c9070
unknown <mtmartincarrizo@gmail.com>
2025-08-04 01:10:39 -0300

    a ver ahora

--- SibomSacks-frontend/src/services/axios.config.js
+++ SibomSacks-frontend/src/services/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instancia = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: import.meta.env.VITE_API_URL, // Esto lee la variable definida en Render
   headers: {
     "Content-Type": "application/json"
   }

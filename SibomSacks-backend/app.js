c4851ad6c814a73264631569282fed4c90df7514
unknown <mtmartincarrizo@gmail.com>
2025-08-05 13:25:47 -0300

    Sibomsacks v1.0

--- SibomSacks-backend/app.js
+++ SibomSacks-backend/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://sibomsacks-1.onrender.com"
+  "https://sibomsacks-gvfz.onrender.com"
 ];
 
 app.use(cors({
